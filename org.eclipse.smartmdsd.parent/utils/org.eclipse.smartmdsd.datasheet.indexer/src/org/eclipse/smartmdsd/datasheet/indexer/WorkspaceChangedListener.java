1d1ea90d5804551d05fcc3ef631dca3382ff01ed
Alex Lotz <lotz@hs-ulm.de>
2020-11-09 10:37:39 +0100

    update headers

--- org.eclipse.smartmdsd.parent/utils/org.eclipse.smartmdsd.datasheet.indexer/src/org/eclipse/smartmdsd/datasheet/indexer/WorkspaceChangedListener.java
+++ org.eclipse.smartmdsd.parent/utils/org.eclipse.smartmdsd.datasheet.indexer/src/org/eclipse/smartmdsd/datasheet/indexer/WorkspaceChangedListener.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright (c) 2019 Technische Hochschule Ulm, Servicerobotics Ulm, Germany
+ * headed by Prof. Dr. Christian Schlegel
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *   Alex Lotz, Dennis Stampfer, Matthias Lutz
+ ********************************************************************************/
 package org.eclipse.smartmdsd.datasheet.indexer;
 
 import org.eclipse.core.resources.IProject;
