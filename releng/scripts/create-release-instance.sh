0f7fea4d01495eb1ea2674d5c711c7900b9d37ba
Alex Lotz <lotz@hs-ulm.de>
2021-03-09 11:55:42 +0100

    add release instance creation bash script

---
+++ releng/scripts/create-release-instance.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+if [[ -z "$PLATFORM" ]]
+then
+  PLATFORM=2020-09
+  echo "Using default Eclipse platform: $PLATFORM"
+else
+  echo "Using Eclipse platform: $PLATFORM"
+fi
+
+
+if [[ -z "$WORKSPACE" ]]
+then
+  WORKSPACE=$PWD
+  echo "Using the current directory as WORKSPACE: $WORKSPACE"
+else
+  echo "Using the WORKSPACE: $WORKSPACE"
+fi
+
+# parse the currently build release version from main feature jar
+JAR_PREFIX="org.eclipse.smartmdsd.toolchain_"
+FEATURE_JAR_PATH=`find $WORKSPACE -name ${JAR_PREFIX}*.jar`
+if [ -z $FEATURE_JAR_PATH ]
+then
+  echo "${JAR_PREFIX}feature jar not found => abort script"
+  exit -1
+fi
+FEATURE_JAR=`basename $FEATURE_JAR_PATH`
+# version length is expected tobe 4 chars long (e.g. 3.15)
+RELEASE_VERSION=${FEATURE_JAR:${#JAR_PREFIX}:4}
+
+# this is the output directory name
+OUTPUT_DIRECTORY="eclipse-smartmdsd-v${RELEASE_VERSION}-${PLATFORM}"
+
+# this is the SmartMDSD P2 repository URL where the SmartMDSD plugins are searched for installation
+SMARTMDSD_REPO_URL="https://download.eclipse.org/smartmdsd/updates/releases/v${RELEASE_VERSION}/${PLATFORM}/"
+
+# this is the main SmartMDSD feature that will be installed
+SMARTMDSD_FEATURE="org.eclipse.smartmdsd.toolchain.feature.group"
+
+# the name of the Eclipse Modeling Tools archive
+ECLIPSE_MODELING_TOOLS_ARCHIVE="eclipse-modeling-${PLATFORM}-R-linux-gtk-x86_64.tar.gz"
+
+# this is the full download URL for downloading the Eclipse Modeling Tools archive
+ECLIPSE_DOWNLOAD_URL="http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/${PLATFORM}/R/${ECLIPSE_MODELING_TOOLS_ARCHIVE}"
+
+# this is the base Eclipse repository URL used to find all additionally required dependencies for installation
+ECLIPSE_REPO_URL="https://download.eclipse.org/releases/${PLATFORM}"
+
+# these are the default eclipse arguments used for installing plugins
+ECLIPSE_ARGUMENST="-noSplash -application org.eclipse.equinox.p2.director -repository $ECLIPSE_REPO_URL"
+
+CMAKE_EDITOR_REPO_URL="https://raw.githubusercontent.com/15knots/cmakeed/master/cmakeed-update/"
+CMAKE_EDITOR_FEATURE="com.cthing.cmakeed.feature.feature.group"
+
+echo "downloading Eclipse Modeling Tools ${PLATFORM}"
+if [[ -z "$JENKINS_HOME" ]]
+then
+  # non-jenkins download (only if file newer than local)
+  wget -N $ECLIPSE_DOWNLOAD_URL
+else
+  wget -nv $ECLIPSE_DOWNLOAD_URL
+fi
+
+echo "extracting eclipse modeling tools archive..."
+tar -xzf eclipse-modeling-${PLATFORM}-R-linux-gtk-x86_64.tar.gz
+
+echo "creating the output directory: ${OUTPUT_DIRECTORY}"
+rm -rf ${OUTPUT_DIRECTORY}
+mv eclipse ${OUTPUT_DIRECTORY}
+
+cd ${OUTPUT_DIRECTORY}
+
+echo "installing SmartMDSD Toolchain feature..."
+./eclipse $ECLIPSE_ARGUMENST -repository $SMARTMDSD_REPO_URL -installIU $SMARTMDSD_FEATURE
+
+echo "installing CMake editor extension..."
+./eclipse $ECLIPSE_ARGUMENST -repository $CMAKE_EDITOR_REPO_URL -installIU $CMAKE_EDITOR_FEATURE
+
+cd ..
+
+echo "creating a tar.gz archive for the installed Eclipse instance..."
+tar -czf ${OUTPUT_DIRECTORY}.tar.gz ${OUTPUT_DIRECTORY}
+
+echo "ALL FINISHED! SmartMDSD Toolchain is installed into the folder $OUTPUT_DIRECTORY"
